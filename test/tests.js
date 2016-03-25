b456df0f8151a7c070a1f9cbf4d939d29b6eb657
Spadon <m.terdjimi@gmail.com>
2016-03-25 11:45:10 +0100

    Updated tests, added asawoo ontology

--- test/tests.js
+++ test/tests.js
@@ -15,7 +15,7 @@ var owl, ontology, mimeType;
 
 var a, b, c;
 
-var reasoningMethod = process.env.rm, ontologyFilename = '/ontologies/fipa.ttl';
+var reasoningMethod = process.env.rm, ontologyFilename = '/ontologies/functionalities.jsonld';
 
 describe('File access', function () {
     it('should access the file', function () {
